71d568f6cf588a092142a2c681c75edf74245a61
Eduardo Alvarez <el_alienx@hotmail.com>
2021-03-31 09:49:28 +0200

    Installed the router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // Project files
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Video from "./pages/Video";
 import "./styles/style.css";
@@ -6,7 +7,12 @@ import "./styles/style.css";
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={Video} path="/video" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
